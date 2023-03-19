0afb1f4f16bfbd3cf205b18b5f24dd2e8f3acef7
jhulyamfebr@gmail.com <jhulyambrozini@gmail.com>
2023-03-18 21:19:05 -0300

    feat: criando validação

---
+++ form-validation/README.md
@@ -0,0 +1,13 @@
+# Validação de formulário
+O presente projeto foi desenvolvido afim de expandir meu aprendizado em HTML5, CSS3 e JavaScript.
+
+## Tecnologias utilizadas
+- HTML5
+- CSS3
+- JavaScript
+
+## Layout
+- Foi utilizado o layout desse projeto: [javascript-form](https://github.com/gabrieldiasss/javascript-form) como base.
+
+### Layout final
+![layout](./assets/layout.png)
---
+++ form-validation/js/validarcpf.js
@@ -0,0 +1,51 @@
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+           value: cpfEnviado.replace(/\D+/g, '')
+        })
+    }
+
+    valida () {
+
+
+       if(typeof this.cpfLimpo === 'undefined') return false // se so tem numeros
+       if(this.cpfLimpo.length !== 11) return false
+       if(this.isSequencia() ) return false
+       this.geraNovoCpf()
+     
+       return this.novoCpf === this.cpfLimpo
+    }
+
+    geraNovoCpf () {
+        const cpfParcial = this.cpfLimpo.slice(0, -2)
+        const digito1 = ValidaCPF.criaDigito(cpfParcial)
+        const digito2 = ValidaCPF.criaDigito(cpfParcial + digito1)
+        this.novoCpf = cpfParcial + digito1 + digito2
+    }
+
+    static criaDigito (cpfParcial) {
+        const cpfArray = Array.from(cpfParcial) // ARRAY com cpf sem dois ultimos digitos
+        let regressivo = cpfArray.length + 1
+    
+        let total = cpfArray.reduce((ac, val) => {
+            ac += (regressivo * Number(val)) // multiplicando os numeros do cpf por um contador regressivo
+            regressivo--
+            return ac
+        },0)
+    
+        const digito = 11 - (total % 11) // digito resultado
+        return digito > 9 ? 0 : String(digito) // nao permetindo que os digitos sejam maiores ou iguais a 10
+    }
+
+    isSequencia() {
+        const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+        return sequencia === this.cpfLimpo
+    }
+
+}
+
+const cpf = new ValidaCPF('070.987.720-03')
+console.log(cpf.valida())
+
+const cpf1 = new ValidaCPF('111.111.111-11')
+console.log(cpf1.valida())
