c4ada3c9910f8634dd283627a917d6eca3244c8f
jhulyamfebr@gmail.com <jhulyambrozini@gmail.com>
2023-04-16 11:40:23 -0300

    feat: consumindo api

---
+++ pagina-clima/index.js
@@ -0,0 +1,70 @@
+const apiKey = '8d482118cf2697fa16cd065e67090f92'
+
+const cityInput = document.querySelector('#city-input')
+const citySearch = document.querySelector('#search')
+
+const cityName = document.querySelector('#city-name')
+const cityCountry = document.querySelector('#city-country')
+const tempNow = document.querySelector('#temp-now')
+const tempMax = document.querySelector('#temp-max')
+const tempMin = document.querySelector('#temp-min')
+const feelsLike = document.querySelector('#feels-like')
+const wind = document.querySelector('#wind-velocity')
+
+const desc = document.querySelector('#description')
+const humidity = document.querySelector('#humidity')
+const clouds = document.querySelector('#clouds')
+
+const sunrise = document.querySelector('#sunrise')
+const sunset = document.querySelector('#sunset')
+const timeNow = document.querySelector('#time-now')
+
+const getDataWeather = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+  const res = await fetch(apiWeatherURL)
+  const data = await res.json()
+
+  return data
+}
+
+const showWeatherData = async (city) => {
+  const data = await getDataWeather(city)
+  console.log(data)
+
+  cityName.innerText = data.name;
+  cityCountry.innerText = data.sys.country
+  tempNow.innerText = parseInt(data.main.temp)
+  tempMax.innerText = parseInt(data.main.temp_max)
+  tempMin.innerText = parseInt(data.main.temp_min)
+  wind.innerText = data.wind.speed
+  feelsLike.innerText = parseInt(data.main.feels_like)
+
+  desc.innerText = data.weather[0].description
+  clouds.innerText = data.clouds.all
+
+  const sunriseData = data.sys.sunrise
+  const sunriseTime = new Date(sunriseData * 1000)
+  const hourSunrise = sunriseTime.getHours().toString().padStart(2, 0)
+  const minutesSunrise = sunriseTime.getMinutes().toString().padStart(2, 0)
+
+  const sunsetData = data.sys.sunset
+  const sunsetTime = new Date(sunsetData * 1000)
+  const hourSunset = sunsetTime.getHours().toString().padStart(2, 0)
+  const minutesSunset = sunsetTime.getMinutes().toString().padStart(2, 0)
+
+  const time = data.dt
+  const timerNow = new Date(time * 1000)
+  const hourTimer = timerNow.getHours().toString().padStart(2, 0)
+  const minutesTimer = timerNow.getMinutes().toString().padStart(2, 0)
+
+  sunrise.innerText = `${hourSunrise}:${minutesSunrise}`
+  sunriseTime.innerText = `${hourSunset}:${minutesSunset}`
+  timeNow.innerText = `${hourTimer}:${minutesTimer}`
+
+}
+
+
+citySearch.addEventListener('click', () => {
+  const city = cityInput.value
+  showWeatherData(city)
+})
